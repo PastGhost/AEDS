1c5a72fe59b27e09cf4124bcade93123a0c7d11c
PastGhost <felipemachadocordeiro@gmail.com>
2017-07-05 10:00:48 -0300

    rnd

---
+++ tp3/entrada/rnd.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <limits.h>
+
+int main() {
+	srand(time(NULL));
+	long l = 1000000000;
+	// long m;
+	// int *x = calloc(l,sizeof(int));
+	// int *y = calloc(l,sizeof(int));
+
+	// for(long i = 0; i < l/10;i++) {
+	// 	m = rand() % l + 1;
+	// 	while(x[m] == 1 )
+	// 		m = rand() % l;
+	// 	x[m] = 1;
+	// 	printf("%ld ",m);
+	// 	m = rand() % l + 1;
+	// 	while(y[m] == 1 )
+	// 		m = rand() % l;
+	// 	y[m] = 1;
+	// 	printf("%ld\n",m);
+
+	// }
+
+	for(long i = 0; i < l/10;i++) {
+		printf("%ld %ld\n",i,l - i);
+
+	}
+
+
+	return 0;
+}
